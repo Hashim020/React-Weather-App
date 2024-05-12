6fa9e6ec5d5ca1d93ccdfacc08c6fe6aadd13e06
Hashim M A <ma.hashim200@gmail.com>
2024-05-12 12:24:38 +0530

    completed

---
+++ src/api.js
@@ -0,0 +1,15 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fb3074a9bbmsh366f07a3687a0f3p1934c3jsne2559355bbfa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY ="cdcf28edccb65db633fd9df512f93762";
